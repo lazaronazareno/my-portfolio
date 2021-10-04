2d1dfb4336f1cb47dca6e43b26c1cfb74343b3a8
lazaronazareno <lazaronazareno@gmail.com>
2021-10-04 16:45:21 -0300

    new font, new welcome text,

--- src/homePages/welcome/index.js
+++ src/homePages/welcome/index.js
@@ -7,7 +7,7 @@ import arrow from '../../static/images/arrow.png'
 function Welcome() {
   return (
     <div className="container-fluid d-flex flex-column text-light welcome-container pt-5 justify-content-center align-items-center">
-      <span className="welcome-container__title">HOLA! SOY LAZARO</span>
+      <span className="welcome-container__title">LAZARO VEGA SANCHEZ</span>
       <span className="welcome-container__text">FRONTEND WEB DEVELOPER JR</span>
       <div className="welcome-container__icons">
         <img className="welcome-container__icon" src={github} alt="Github" />
