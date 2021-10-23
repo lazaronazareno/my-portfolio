7178908e924c34f233ec7a3b4efef8c6401f3c74
lazaronazareno <lazaronazareno@gmail.com>
2021-10-22 23:00:22 -0300

    new-styles-slider-page-alternative-done

--- src/homePages/alternative/index.js
+++ src/homePages/alternative/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './styles.scss'
 
 function BonusPage() {
   return (
     <div className="container-fluid d-flex flex-column bonus-container justify-content-center align-items-center">
-      <h1>bonus track</h1>
-      <span>Una pagina excentrica que me gustaria hacer en un futuro de mejor manera, más "elegante", que a medida que avance mis conocimientos pueda mejorar y hacerla mi pagina principal</span>
-      <button className="bonus-container__button btn btn-outline-dark d-flex align-self-center">Ir a la pagina alternativa</button>
+      <h1 className="bonus-container__title">Bonus Track</h1>
+      <span className="p-2 text-center bonus-container__text">Una pagina excentrica que me gustaria hacer en un futuro de mejor manera, más "elegante", que a medida que avance mis conocimientos pueda mejorar y hacerla mi pagina principal</span>
+      <Link to="/alternative" className="bonus-container__button btn btn-outline-dark btn-lg">Ir a la pagina alternativa</Link>
     </div>
   )
 }
